49ee03f49d4bc77ec898cc5f61705448bd074915
Shawn <43222123+1Shawn1@users.noreply.github.com>
2022-07-08 15:46:50 -0500

    Added multi-account support
    would be buggy when running 2 accounts due to it being impossible to have different settings due to it using global env

--- Autofarm.lua
+++ Autofarm.lua
@@ -21,12 +21,12 @@ end
 local am = 0
 local function AutoEgg()
     while wait(0.1) do
-        if getgenv().Settings.AutoEgg.Enabled then
+        if getgenv().Settings[game.Players.LocalPlayer.UserId].AutoEgg.Enabled then
             for i = 1, 3 do
-                game:GetService("ReplicatedStorage").Remotes.BuyEgg:FireServer(getgenv().Settings.AutoEgg.Egg)
+                game:GetService("ReplicatedStorage").Remotes.BuyEgg:FireServer(getgenv().Settings[game.Players.LocalPlayer.UserId].AutoEgg.Egg)
             end
-            if am > 100 and getgenv().Settings.AutoEgg.AutoFuse.Enabled then
-                game:GetService("ReplicatedStorage").Remotes.AutoFuse:FireServer({[1] = getgenv().Settings.AutoEgg.AutoFuse.Common,[2] = getgenv().Settings.AutoEgg.AutoFuse.Uncommon,[3] = getgenv().Settings.AutoEgg.AutoFuse.Rare,[4] = getgenv().Settings.AutoEgg.AutoFuse.Epic,[5] = getgenv().Settings.AutoEgg.AutoFuse.Legendary,[6] = getgenv().Settings.AutoEgg.AutoFuse.Prodigious,[7] = getgenv().Settings.AutoEgg.AutoFuse.Ascended,[8] = getgenv().Settings.AutoEgg.AutoFuse.FuseEquipPets})
+            if am > 100 and getgenv().Settings[game.Players.LocalPlayer.UserId].AutoEgg.AutoFuse.Enabled then
+                game:GetService("ReplicatedStorage").Remotes.AutoFuse:FireServer({[1] = getgenv().Settings[game.Players.LocalPlayer.UserId].AutoEgg.AutoFuse.Common,[2] = getgenv().Settings[game.Players.LocalPlayer.UserId].AutoEgg.AutoFuse.Uncommon,[3] = getgenv().Settings[game.Players.LocalPlayer.UserId].AutoEgg.AutoFuse.Rare,[4] = getgenv().Settings[game.Players.LocalPlayer.UserId].AutoEgg.AutoFuse.Epic,[5] = getgenv().Settings[game.Players.LocalPlayer.UserId].AutoEgg.AutoFuse.Legendary,[6] = getgenv().Settings[game.Players.LocalPlayer.UserId].AutoEgg.AutoFuse.Prodigious,[7] = getgenv().Settings[game.Players.LocalPlayer.UserId].AutoEgg.AutoFuse.Ascended,[8] = getgenv().Settings[game.Players.LocalPlayer.UserId].AutoEgg.AutoFuse.FuseEquipPets})
                 am = 0
             else
                 am++
@@ -37,10 +37,10 @@ end
 
 local function AutoQuest()
     while task.wait() do
-        if getgenv().Settings.AutoQuest and game:GetService("Players").LocalPlayer.QuestArea.Value ~= 0 then
+        if getgenv().Settings[game.Players.LocalPlayer.UserId].AutoQuest and game:GetService("Players").LocalPlayer.QuestArea.Value ~= 0 then
             Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Areas["Area" .. game:GetService("Players").LocalPlayer.QuestArea.Value].CFrame * CFrame.new(0, 4, 0)
         end
-        if getgenv().Settings.AutoClaimQuest and game:GetService("Players").LocalPlayer.PlayerGui.ScreenGui.Main.Top.QuestFrame.Checkmark.Check.Visible then
+        if getgenv().Settings[game.Players.LocalPlayer.UserId].AutoClaimQuest and game:GetService("Players").LocalPlayer.PlayerGui.ScreenGui.Main.Top.QuestFrame.Checkmark.Check.Visible then
             game:GetService("ReplicatedStorage").Remotes.ClaimQuestReward:FireServer()
         end
     end
